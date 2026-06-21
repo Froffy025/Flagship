9cc08f979e0065664cefc9a8a4f9ad7e455de963
JohnJohn <189290423+JohnJJohn@users.noreply.github.com>
2026-06-21 07:32:08 +0100

    Modules (#2)
    * forgot
    
    Modules
    
    * larp
    
    * huh
    
    * git broke
    
    ---------
    
    Co-authored-by: deltanedas <39013340+deltanedas@users.noreply.github.com>

---
+++ Content.FlagShip.Server/Program.cs
@@ -0,0 +1,11 @@
+using Robust.Server;
+
+namespace Content.FlagShip.Client;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        ContentStart.Start(args);
+    }
+}
